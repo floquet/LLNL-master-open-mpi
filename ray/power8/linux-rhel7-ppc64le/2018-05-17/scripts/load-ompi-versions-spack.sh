3127824f8850ca110690f70fbcb13b7c57558be5
Daniel Topa <dantopa@lanl.gov>
2018-05-20 18:33:39 -0700

    ray power8 builds complete

---
+++ ray/power8/linux-rhel7-ppc64le/2018-05-17/scripts/load-ompi-versions-spack.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+printf '%s\n' "+++  $(pwd)/${BASH_SOURCE[0]} $(date)"
+
+export v_ompi=""
+## 3
+export v_ompi="${v_ompi} 3.1.0 3.0.1 3.0.0"
+## 2
+export v_ompi="${v_ompi} 2.1.3 2.1.2  2.1.1  2.1.0  2.0.4  2.0.3  2.0.2  2.0.1  2.0.0"
+## 1
+export v_ompi="${v_ompi} 1.10.7"
+# export v_ompi="${v_ompi} 1.10.7 1.10.6 1.10.5 1.10.4 1.10.3 1.10.2 1.10.1 1.10.0"
+# export v_ompi="${v_ompi} 1.8.8  1.8.7  1.8.6  1.8.5  1.8.4  1.8.3  1.8.2  1.8.1  1.8"
+# export v_ompi="${v_ompi} 1.7.5  1.7.4  1.7.3  1.7.2  1.7.1  1.7"
+# export v_ompi="${v_ompi} 1.6.5  1.6.4  1.6.3  1.6.2  1.6.1  1.6"
+# export v_ompi="${v_ompi} 1.5.5  1.5.4  1.5.3  1.5.2  1.5.1  1.5"
+# export v_ompi="${v_ompi} 1.4.5  1.4.4  1.4.3  1.4.2  1.4.1  1.4"
+# export v_ompi="${v_ompi} 1.3.4  1.3.3  1.3.2  1.3.1  1.3"
+# export v_ompi="${v_ompi} 1.2.9  1.2.8  1.2.7  1.2.6  1.2.5  1.2.4  1.2.3  1.2.2  1.2.1  1.2"
+# export v_ompi="${v_ompi} 1.1.5  1.1.4  1.1.3  1.1.2  1.1.1  1.1"
+# export v_ompi="${v_ompi} 1.0.2  1.0.1  1.0"
+
+census=( ${v_ompi} )
+echo "${#census[@]} versions of Open MPI are loaded"
+# echo "latest version   = ${v_ompi[1]}"
+# echo "earliest version = ${v_ompi[-1]}"
+#
+# for v in ${v_ompi}; do
+#     echo "verion ${v}"
+# done
+
+# dantopa@ccscs4.lanl.gov:spack.current.ccscs4 $ date
+# Tue May  8 16:23:34 MDT 2018
+
+# dantopa@ccscs4.lanl.gov:spack.current.ccscs4 $ pwd
+# /scratch/dantopa/spack.current.ccscs4
+
+# dantopa@ccscs4.lanl.gov:spack.current.ccscs4 $ spack versions openmpi
+# ==> Warning: Spack will not check SSL certificates. You need to update your Python to enable certificate verification.
+# ==> Warning: Spack will not check SSL certificates. You need to update your Python to enable certificate verification.
+# ==> Safe versions (already checksummed):
+#   3.0.1  2.1.2  2.0.4  2.0.1   1.10.6  1.10.3  1.10.0  1.8.6  1.8.3  1.8    1.7.3  1.7    1.6.3  1.6    1.5.3  1.5    1.4.3  1.4    1.3.2  1.2.9  1.2.6  1.2.3  1.2    1.1.3  1.1    1.0
+#   3.0.0  2.1.1  2.0.3  2.0.0   1.10.5  1.10.2  1.8.8   1.8.5  1.8.2  1.7.5  1.7.2  1.6.5  1.6.2  1.5.5  1.5.2  1.4.5  1.4.2  1.3.4  1.3.1  1.2.8  1.2.5  1.2.2  1.1.5  1.1.2  1.0.2
+#   2.1.3  2.1.0  2.0.2  1.10.7  1.10.4  1.10.1  1.8.7   1.8.4  1.8.1  1.7.4  1.7.1  1.6.4  1.6.1  1.5.4  1.5.1  1.4.4  1.4.1  1.3.3  1.3    1.2.7  1.2.4  1.2.1  1.1.4  1.1.1  1.0.1
+# ==> Remote versions (not yet checksummed):
+#   3.1.0  3.0.1rc4  3.0.1rc3  3.0.1rc2  3.0.0rc5
---
+++ ray/power8/linux-rhel7-ppc64le/2018-05-17/scripts/load-spack-compilers-ray-power8.sh
@@ -0,0 +1,57 @@
+#!/bin/bash
+printf '%s\n' "+++  $(pwd)/$(tput bold)${BASH_SOURCE[0]}$(tput sgr0) $(date)"
+
+# topa1@ray23:scripts $ date
+# Wed May 16 16:40:06 PDT 2018
+
+# topa1@ray23:scripts $ pwd
+# /g/g13/topa1/spack/spack.ray.current/scripts
+
+# topa1@ray23:scripts $ spack arch
+# linux-rhel7-ppc64le
+
+# topa1@ray23:scripts $ gcc --version
+# gcc (GCC) 4.9.3
+
+export SECONDS=0
+
+l_compilers=""
+## clang
+l_compilers="${l_compilers} clang@4.0.0"
+l_compilers="${l_compilers} clang@3.9.1  clang@3.8.0"
+## gcc
+l_compilers="${l_compilers} gcc@8.1.0"
+l_compilers="${l_compilers} gcc@7.3.0  gcc@7.2.0  gcc@7.1.0"
+l_compilers="${l_compilers} gcc@6.4.0  gcc@6.3.0  gcc@6.2.0  gcc@6.1.0"
+l_compilers="${l_compilers} gcc@5.5.0  gcc@5.4.0  gcc@5.3.0  gcc@5.2.0  gcc@5.1.0"
+l_compilers="${l_compilers} gcc@4.9.4  gcc@4.9.3  gcc@4.9.2  gcc@4.8.5  gcc@4.8.4"
+## xl
+l_compilers="${l_compilers} xl@16.1    xl@15.1    xl@13.1"
+l_compilers="${l_compilers} xl_r@16.1  xl_r@15.1  xl_r@13.1"
+
+census=( ${l_compilers} )
+echo "${#census[@]} Spack-recognized compilers loaded on ${host_name}"
+
+# topa1@ray23:spack.ray.current $ date
+# Thu May 17 11:28:51 PDT 2018
+
+# topa1@ray23:spack.ray.current $ pwd
+# /g/g13/topa1/spack/spack.ray.current
+
+# topa1@ray23:spack.ray.current $ spack arch
+# linux-rhel7-ppc64le
+
+# topa1@ray23:spack.ray.current $ spack compilers
+# ==> Available compilers
+# -- clang rhel7-ppc64le ------------------------------------------
+# clang@4.0.0  clang@3.9.1  clang@3.8.0
+#
+# -- gcc rhel7-ppc64le --------------------------------------------
+# gcc@8.1.0  gcc@7.2.0  gcc@6.4.0  gcc@6.2.0  gcc@5.5.0  gcc@5.3.0  gcc@5.1.0  gcc@4.9.3  gcc@4.8.5
+# gcc@7.3.0  gcc@7.1.0  gcc@6.3.0  gcc@6.1.0  gcc@5.4.0  gcc@5.2.0  gcc@4.9.4  gcc@4.9.2  gcc@4.8.4
+#
+# -- xl rhel7-ppc64le ---------------------------------------------
+# xl@16.1  xl@15.1  xl@13.1
+#
+# -- xl_r rhel7-ppc64le -------------------------------------------
+# xl_r@16.1  xl_r@15.1  xl_r@13.1
